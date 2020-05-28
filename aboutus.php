689b6fb795e1a1fd79669abfa0ad85bb6942d940
chirs21 <chiragmalpani21@gmail.com>
2020-05-28 19:45:09 +0530

    added new files

---
+++ aboutus.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ABOUT US</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style_navbar.css">
+<link rel="stylesheet" type="text/css" href="css/style_footer.css">
+<?php
+	require("includes/header.php");
+	require("includes/navbar.php");
+?>
+<style type="text/css">
+	.myrow{
+		text-align: center;
+	}
+	.logo{
+		display: inline-block;
+		align-items: center;
+	}
+	.paragraph{
+		background-color: #f0f0f0;
+		/*height: 300px;*/
+	}
+	.row_para{
+		padding-top: 1rem;
+		padding-bottom: 3rem;
+	}
+	.paragraph{
+		border-radius: 1.5rem;
+		padding: 1rem;
+		justify-content: center;
+	}
+</style>
+<body>
+	<p><h2 class="display1"><center>About Us</center></h3></p>
+		<div class="container-fluid myrow">
+		<div class="logo">
+			<img src="images/00.jpg" style="width:350px;height: 300px">
+		</div>
+		<div class="row row_para">
+			<div class="col-md-3"></div>
+			<div class="col-md-6 paragraph">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col-md-3"></div>
+		</div>
+		</div>
+</body>
+<?php
+require("includes/footer.php");
+?>
+</html>
---
+++ terms.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Terms & Conditions</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/style_navbar.css">
+<link rel="stylesheet" type="text/css" href="css/style_footer.css">
+<style type="text/css">
+	.display1{
+		padding-left: 13rem;
+	}
+	.container_terms{
+		background-color: #f0f0f0;
+		border-radius: 25px;
+		margin-bottom: 2rem;
+		justify-content: center;
+		text-align: center;
+		padding: 2rem;
+	}
+</style>
+<?php
+	require("includes/header.php");
+	require("includes/navbar.php");
+?>
+<body>
+	<p><h1 class="display1">Terms & Conditions</h1></p>
+	<br>
+	<div class="container container_terms">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</body>
+	<?php
+	require("includes/footer.php");
+	?>
+</html>
