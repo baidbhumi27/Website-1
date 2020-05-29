5f1ff1a409316460672207a966b1bda6ee02ee76
baidbhumi27 <baidbhumi27@gmail.com>
2020-05-29 15:55:19 +0530

    post.php layout done designing left

---
+++ forms/form1.php
@@ -0,0 +1,59 @@
+<div class="container">
+  <form>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">First Name</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="country">Industry</label>
+      </div>
+      <div class="col-75">
+        <select class="tab_form_input form-control" id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="country">Type Of Company</label>
+      </div>
+      <div class="col-75">
+        <select class="tab_form_input form-control" id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="country">Locations</label>
+      </div>
+      <div class="col-75">
+        <select class="tab_form_input form-control" id="country" name="country">
+          <option value="australia">Australia</option>
+          <option value="canada">Canada</option>
+          <option value="usa">USA</option>
+        </select>
+      </div>
+    </div>  
+    <br>  
+    <div class="tab_row float-left">
+      <input class="tab_form_submit" type="submit" value="Update">
+    </div>
+    <div class="tab_row float-right">
+      <input class="tab_form_submit" type="submit" value="Next">
+    </div>  
+  </form>
+</div>
---
+++ forms/form3.php
@@ -0,0 +1,57 @@
+<div class="container">
+  <form>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">Facebook Profile Link</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">Instagram Profile Link</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">Twitter Profile Link</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">You Tube Link</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <div class="tab_row">
+      <div class="col-25">
+        <label class="tab_form_label" for="fname">Linkedin Profile Link</label>
+      </div>
+      <div class="col-75">
+        <input class="tab_form_input form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+      </div>
+    </div>
+      
+    <br>  
+    <div class="tab_row float-left">
+      <input class="tab_form_submit" type="submit" value="Update">
+    </div>
+    <div class="tab_row float-right">
+      <input class="tab_form_submit" type="submit" value="Next">
+    </div>  
+  </form>
+</div>
